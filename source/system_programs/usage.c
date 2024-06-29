b7ab4d5e98bd14f171be0435a19ec4b1792349a7
taniakohtzeern2 <126877006+taniakohtzeern2@users.noreply.github.com>
2024-06-29 21:59:35 +0800

    Update usage.c

--- source/system_programs/usage.c
+++ source/system_programs/usage.c
@@ -3,34 +3,25 @@
 #include <string.h>
 
 //set up array of descriptions
-const char *builtin_desc[] = {
-    "cd directory_name to change working directory of the shell",
-    "help for supported commands",
-    "exit to terminate the shell gracefully",
-    "usage cd/help/exit",
-    "env to list all registered env variables",
-    "set env ENV=VALUE to set a new env variable",
-    "unsetenv ENV to remove this env from the list of env variables",
-};
+int num_builtin_functions()
+{
+    return sizeof(builtin_commands) / sizeof(char *);
+}
 
-//function to print fstring based on function(args[1])
-void print_description(const char *function) {
-    if (strcmp(function, "cd") == 0) {
-        printf("%s\n", builtin_desc[0]);
-    } else if (strcmp(function, "help") == 0) {
-        printf("%s\n", builtin_desc[1]);
-    } else if (strcmp(function, "exit") == 0) {
-        printf("%s\n", builtin_desc[2]);
-    } else if (strcmp(function, "usage") == 0) {
-        printf("%s\n", builtin_desc[3]);
-    } else if (strcmp(function, "env") == 0) {
-        printf("%s\n", builtin_desc[4]);
-    } else if (strcmp(function, "set") == 0) {
-        printf("%s\n", builtin_desc[5]);
-    } else if (strcmp(function, "unsetenv") == 0) {
-        printf("%s\n", builtin_desc[6]);
-    } else {
-        printf("The command you gave %s, is not part of CSEShell's builtin command\n", function);
+
+void print_description(const char *function){
+    //create for loop to input correct desc based on command input
+    int found=0; //add this as a flag to see if command is found
+    for (int i=0; i<num_builtin_functions();i++){
+        if (strcmp(function, builtin_commands[i]) == 0){
+            printf("%s\n", builtin_desc[i]);
+            found=1;
+            break;
+        }
+    }
+    //return error if not found
+    if (!found) {
+        printf("The command you gave %s is not part of CSEShell's builtin command\n", function);
     }
 }
 
