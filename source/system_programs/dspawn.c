69e03fcbc71a1a1dabd9b7dddc35aaa75cb00460
taniakohtzeern2 <tania_koh@mymail.sutd.edu.sg>
2024-06-30 14:18:32 +0800

    reedited dspawn and added dcheck

--- source/system_programs/dspawn.c
+++ source/system_programs/dspawn.c
@@ -16,7 +16,7 @@ static int daemon_work()
     FILE *fptr;
     char *cwd;
     char buffer[1024];
-    printf("PID %d Daemon writing line %d to the file.  \n", getpid(), num);
+    
     // write PID of daemon in the beginning
     fptr = fopen(output_file_path, "a");
     if (fptr == NULL)
@@ -64,36 +64,7 @@ static int daemon_work()
     return EXIT_SUCCESS;
 }
 
-int main(){
-    // in main() of dspawn.c 
-
-    // Setup path
-    if (getcwd(output_file_path, sizeof(output_file_path)) == NULL)
-    {
-        perror("getcwd() error, exiting now.");
-        return 1;
-    }
-    strcat(output_file_path, "/dspawn.log");
-    printf("%s", output_file_path);
-
-    //step 1: first fork, parent and childfork() is so that dspawn returns immediately to cseshell and our shell does not wait for the daemon to exit.
-   pid_t pid = fork();
-   if (pid == 0){
-       printf("Child process with pid %d, pgid %d, session id: %d\n", getpid(), getpgid(getpid()), getsid(getpid()));
-       //creates new session
-       //step 3:tep 3: setsid
-// The child (intermediate process) process is by default not a process group leader. It calls setsid() to be the session leader and loses controlling TTY (terminal).
-       setsid(); // child tries setsid
-       //pgid is process group leader but not sess leader
-       printf("Child process has setsid with pid %d, pgid %d, session id: %d\n", getpid(), getpgid(getpid()), getsid(getpid()));
-
-   }
-   else{
-     printf("Parent process with pid %d, pgid %d, session id :%d\n", getpid(), getpgid(getpid()), getsid(getpid()));
-       setsid(); // parent tries setsid
-       printf("Parent process has setsid with pid %d, pgid %d, session id: %d\n", getpid(), getpgid(getpid()), getsid(getpid()));
-       wait(NULL);
-   }
+int intermediate_process(){
    //step 4
    signal(SIGHUP, SIG_IGN);
    //sigchld 
@@ -113,18 +84,67 @@ int main(){
    umask(0);
    //freopen:change the file associated with a standard text stream (stderr, stdin, or stdout),
    chdir("/");
-   /* Close all open file descriptors */
-   int x;
-   int fd0, fd1, fd2; 
-   for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
-   {
-       close (x);
-   }
+   //daemon process
+   if (pid2==0){
+        //step 6
+        //u mask sets file mode creation mask
+        //determines permissions to newly created files
+        //permission 777: read, write, execute(111 111 111) -> convert to binary
+        //set to 0 -> all files are world-rw and executable
+        umask(0);
+        //freopen:change the file associated with a standard text stream (stderr, stdin, or stdout),
+        chdir("/");
+        /* Close all open file descriptors */
+        int x;
+        int fd0, fd1, fd2; 
+        for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+        {
+            close (x);
+        }
 
-   /*
-   * Attach file descriptors 0, 1, and 2 to /dev/null. */
-   fd0 = open("/dev/null", O_RDWR);
-   fd1 = dup(0);
-   fd2 = dup(0);
-   return daemon_work();
+        /*
+        * Attach file descriptors 0, 1, and 2 to /dev/null. */
+        fd0 = open("/dev/null", O_RDWR);
+        fd1 = dup(0);
+        fd2 = dup(0);
+        int result=daemon_work();
+            if (result !=EXIT_SUCCESS){
+                fprintf(stderr, "Daemon work failed");
+                return 1;
+            }
+            exit(0);
+        } else {
+            exit(0);
+        }
 }
+
+int main(){
+    // in main() of dspawn.c 
+
+    // Setup path
+    if (getcwd(output_file_path, sizeof(output_file_path)) == NULL)
+    {
+        perror("getcwd() error, exiting now.");
+        return 1;
+    }
+    strcat(output_file_path, "/dspawn.log");
+    printf("%s\n", output_file_path);
+
+    //step 1: first fork, parent and childfork() is so that dspawn returns immediately to cseshell and our shell does not wait for the daemon to exit.
+   pid_t pid = fork();
+   if (pid == 0){
+       printf("Child process with pid %d, pgid %d, session id: %d\n", getpid(), getpgid(getpid()), getsid(getpid()));
+       //creates new session
+       //step 3:tep 3: setsid
+// The child (intermediate process) process is by default not a process group leader. It calls setsid() to be the session leader and loses controlling TTY (terminal).
+       setsid(); // child tries setsid
+       //pgid is process group leader but not sess leader
+   }
+   else{
+       setsid(); // parent tries setsid
+       wait(NULL);
+       intermediate_process();
+   }
+   return 0;
+}
+
